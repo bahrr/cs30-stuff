f73ae03a5d9f44bff83efa47edba992dffb80e11
Ivan <kivaniovich@gmail.com>
2023-03-07 15:20:26 -0600

    did some stuff

--- cubic disarray/sketch.js
+++ cubic disarray/sketch.js
@@ -5,11 +5,40 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let boxes = [];
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  rectMode(CENTER);
+}
+
+function drawBox(box) {
+  push();
+  translate(box.x, box.y);
+  rotate(box.r);
+  square(0, 0, box.s);
+  pop();
+}
+
+function spawnBox(theX, theY, theSize, howRotated) {
+  let someBox = {
+    x: theX,
+    y: theY,
+    s: theSize,
+    r: howRotated
+  };
+  boxes.push(someBox);
 }
 
 function draw() {
   background(220);
+  for (let y = 0; y < height; y += 50) {
+    for (let x = 0; x < width; x += 50) {
+      spawnBox(x, y, 50, random(y / mouseY));
+    }
+  }
+ for (let i = 0; i < boxes.length; i++) {
+    drawBox(boxes[i]);
+  }
 }
