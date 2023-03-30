acaf4df064b2231fafcd9e1c6664159baf2756b3
Ivan <kivaniovich@gmail.com>
2023-03-30 15:03:56 -0600

    some sudoku stuff

--- 2d-arrays/sketch.js
+++ 2d-arrays/sketch.js
@@ -4,63 +4,58 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-// let grid = [[0, 0, 1, 1],
-//             [0, 1, 0, 0],
-//             [0, 0, 1, 0],
-//             [0, 1, 0, 0]];
+let grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 3, 0, 8, 5],
+            [0, 0, 1, 0, 2, 0, 0, 0, 0],
+            [0, 0, 0, 5, 0, 7, 0, 0, 0],
+            [0, 0, 4, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [5, 0, 0, 0, 0, 0, 0, 7, 3],
+            [0, 0, 2, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 9]];
 
-let grid;
 
-const ROWS = 16;
-const COLS = 16;
-let cellsize;
+const ROWS = 9;
+const COLS = 9;
+let cellSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  grid = createRandomGrid(ROWS, COLS);
 
   if (width < height) {
-    cellsize = width / ROWS;
+    cellSize = width / ROWS;
   }
   else {
-    cellsize = height / ROWS;
+    cellSize = height / ROWS;
   }
-}
-
-function createRandomGrid(ROWS, COLS) {
-  let newGrid = [];
-  for (let y = 0; y < ROWS; y++) {
-    newGrid.push([]);
-    for (let x = 0; x < COLS; x++) {
-      let i = Math.round(random());
-      newGrid[y].push(i);
-    }
-  }
-  return newGrid;
 }
 
 function draw() {
   displayGrid();
+  // drawBoxes();
+}
+
+function drawBoxes() {
+  push();
+  noFill();
+  strokeWeight(5);
+  for (let y = 0; y < 3; y++) {
+    for (let x = 0; x < 3; y++) {
+      square(x * cellSize, y * cellSize, x * 3 *cellSize);
+    }
+  }
+  pop();
 }
 
 function displayGrid() {
   for (let y = 0; y < ROWS; y++) {
     for (let x = 0; x < COLS; x++) {
-      fill(grid[y][x] * 255);
-      square(x * cellsize, y * cellsize, cellsize);
+      fill(255);
+      square(x * cellSize, y * cellSize, cellSize);
+      fill(0);
+      textAlign(CENTER, CENTER);
+      text(grid[y][x], x* cellSize + cellSize / 2,
+        y * cellSize + cellSize / 2);
     }
   }
-}
-
-function keyTyped() {
-  if (key === "r") {
-    grid = createRandomGrid(ROWS, COLS);
-  }
-}
-
-function mousePressed() {
-  let x = Math.floor(mouseX / cellsize);
-  let y = Math.floor(mouseY / cellsize);
-
-  grid[y][x] = (grid[y][x] + 1) % 2;
 }
